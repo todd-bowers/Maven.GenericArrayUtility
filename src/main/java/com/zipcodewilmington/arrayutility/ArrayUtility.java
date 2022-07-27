4230f0c967645a0fef4e31e9f58a7154264b3bf9
todd-bowers <toddbows@gmail.com>
2022-07-26 22:08:29 -0400

    Method stubs created

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,26 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<SomeType> {
+    private final SomeType[] array;
+
+    public ArrayUtility(SomeType[] array) {
+        this.array = array;
+    }
+
+    public Integer countDuplicatesInMerge(SomeType[] arrayToMerge, SomeType valueToEvaluate) {
+        return null;
+    }
+
+    public SomeType getMostCommonFromMerge(SomeType[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(SomeType valueToEvaluate) {
+        return null;
+    }
+
+    public SomeType[] removeValue(SomeType valueToRemove) {
+        return null;
+    }
 }
